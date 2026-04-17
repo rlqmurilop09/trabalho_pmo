716752ed3887f217911f9cd4aad7607b8d943744
mffvs1-creato <mffvs1@aluno.ifal.edu.br>
2026-04-17 09:14:26 -0300

    fiz a classe começo

---
+++ lib/doacoes.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_pmo/doacoes.dart';
+
+class doacoes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PetLar"),
+        backgroundColor: Colors.blue[200],
+      ),
+      body: center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            ElevatedButton(
+              child: Text("Adotar"),
+              onPressed: () {
+              },
+            ),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              child: Text("Cadastrar Animal Encontrado"),
+              onPressed: () {
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
