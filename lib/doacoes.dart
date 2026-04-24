56d016fe3178c7f2097aca36041f75b089297465
C4rol-sudo <acam5@aluno.ifal.edu.br>
2026-04-24 08:43:59 -0300

    editar (Bia)

--- lib/doacoes.dart
+++ lib/doacoes.dart
@@ -52,6 +52,7 @@ class _DoacoesState extends State<Doacoes> {
           ),
         ],
       ),
+
     );
   }
 }
---
+++ lib/homepage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_pmo/doacoes.dart';
+import 'package:projeto_pmo/procura.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  List pages = [
+    Doacoes(),
+    Procura(),
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: pages[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF90CAF9),
+          currentIndex: selectedIndex,
+          showSelectedLabels: true,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Color(0xFF0B42A8),
+          type: BottomNavigationBarType.fixed,
+          onTap: (index){
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home) , label: 'Início'),
+            BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Histórias'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Encontrar'),
+            BottomNavigationBarItem(icon: Icon(Icons.monetization_on), label: 'Doações'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: 'Perfil'
+            ),
+
+          ],
+        )
+    );
+  }
+}
