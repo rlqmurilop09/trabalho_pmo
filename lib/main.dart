972e99105c976403fb485a4db77f9dd45580d5f0
grs18-git <grs18@aluno.ifal.edu.br>
2026-04-28 16:14:43 -0300

    mudei o nome eventos para avisos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_pmo/homepage.dart';
-import 'package:projeto_pmo/eventos.dart';
+import 'package:projeto_pmo/avisos.dart';
 
 void main() {
   //runApp(MaterialApp(home: HomePage()));
