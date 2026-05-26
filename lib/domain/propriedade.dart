44282c1a2f9bb87b46c33c642bea0140631a2197
Aluno <mbbs3@aluno.ifal.edu.br>
2026-05-26 17:31:35 -0300

    organizar tudo

---
+++ lib/domain/propriedade.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Propriedade {
+  String urlImage;
+  String nome;
+  String idade;
+  String genero;
+  String especie;
+  String porte;
+  String raca;
+  String vacinado;
+  String castrado;
+
+  Propriedade({
+    required urlImage;
+    required nome;
+    required idade;
+    required genero;
+    required especie;
+    required porte;
+    required raca;
+    required vacinado;
+    required castrado
+  });
+}
---
+++ lib/widget/container_propriedade.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class ContainerPropriedade extends StatefulWidget {
+  Propriedade propriedade;
+
+  const ContainerPropriedade({super.key});
+
+  @override
+  State<ContainerPropriedade> createState() => _ContainerPropriedadeState();
+}
+
+class _ContainerPropriedadeState extends State<ContainerPropriedade> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Color(0xFF8FB9E3),
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFFC9D4E5),
+            blurRadius: 8,
+            offset: Offset(4, 8),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                child: Image.network(
+                  urlImage,
+                  height: 400,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ],
+          ),
+
+          SizedBox(height: 4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                nome,
+                style: GoogleFonts.inter(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Row(
+                children: [
+                  Text('ONG: Patinha feliz '),
+                  Icon(Icons.pets, size: 20),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 2),
+          Text(genero),
+          Text(idade),
+          Text(especie),
+          Text(porte),
+          Text(raca),
+          SizedBox(height: 8),
+          pin(vacina: vacinado, castrado: castrado),
+        ],
+      ),
+    );
+  }
+
+  pin({required String vacina, required String castrado}){
+    return Row(
+      children: [
+        Container(
+          width: 70,
+          height: 25,
+          decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(16)
+          ),
+
+          child: Center(
+            child: Text(vacina,style: TextStyle(
+                fontSize: 13
+            )),
+          ),
+        ),
+
+        Container(
+          width: 70,
+          height: 25,
+          decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.circular(16)
+          ),
+
+          child: Center(
+            child: Text(castrado,style: TextStyle(
+                fontSize: 13
+            )),
+          ),
+        ),
+      ],
+    );
+  }
+}
+  }
+}
+
+
