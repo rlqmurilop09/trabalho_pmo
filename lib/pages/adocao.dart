13caf8f0e9ad8c6e1f2b2e5646995ab4fcbbc134
Beatriz <mbbs3@aluno.ifal.edu.br>
2026-06-18 16:05:00 -0300

    ADOCAO

---
+++ lib/pages/adocao.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import "package:projeto_pmo/domain/propriedade.dart";
+import 'package:projeto_pmo/db/propriedade_dao.dart';
+import 'package:projeto_pmo/widget/container_adocao.dart';
+
+
+class Adocao extends StatefulWidget {
+  const Adocao({super.key});
+
+
+  @override
+  State<Adocao> createState() => _AdocaoState();
+}
+
+
+class _AdocaoState extends State<Adocao> {
+  List<Propriedade> listaPropriedades = [];
+  PropriedadeDao adocaodao = PropriedadeDao();
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData() async {
+    listaPropriedades = await PropriedadeDao().listarPropriedades();
+    setState(() {
+      var isLoading = false;
+    });
+  }
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Adoção"), backgroundColor: Color(0xFF8FB9E3)),
+      backgroundColor: Color(0xFFBBDEFB),
+      body: ListView.builder(
+        itemCount: listaPropriedades.length,
+        itemBuilder: (context, index) {
+          return ContainerAdocao(
+            propriedade: listaPropriedades[index],
+          );
+        },
+      ),
+    );
+  }
+}
+
