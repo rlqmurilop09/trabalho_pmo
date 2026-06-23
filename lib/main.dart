a9c149a038e0954e699a5c4793088fe0d222c6a9
messi <cgga1@aluno.ifal.edu.br>
2026-06-23 09:04:15 -0300

    APRESENTAÇÃO 23/06 (finalizada) 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_pmo/pages/homepage.dart';
-//import 'package:projeto_pmo/pages/eventos.dart';
 
-//páginas murilo
-import 'package:projeto_pmo/pages/doarmantimentos.dart';
+//clara
+import 'package:projeto_pmo/pages/avisos.dart';
+
+//murilo
 import 'package:projeto_pmo/pages/doardinheiro.dart';
+import 'package:projeto_pmo/pages/registro_doacao.dart';
+
+//beatriz
+import 'package:projeto_pmo/pages/adocao.dart';
+
+//jamylle
+import 'package:projeto_pmo/pages/historias.dart';
 
 void main() {
   runApp(MaterialApp(home: HomePage()));
